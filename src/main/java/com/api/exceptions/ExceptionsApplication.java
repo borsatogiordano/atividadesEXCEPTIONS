0677743922a4b549d2a6880d2337f03e95050c9c
Giordano Borsato <borsatogiordano@gmail.com>
2024-07-17 21:43:16 -0300

    Ex 3 funcionando

--- src/main/java/com/api/exceptions/ExceptionsApplication.java
+++ src/main/java/com/api/exceptions/ExceptionsApplication.java
@@ -10,7 +10,7 @@ public class ExceptionsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExceptionsApplication.class, args);
 		
-		Exercicios.ex2();
+		Exercicios.ex3();
 	}
 
 }
--- src/main/java/com/api/exceptions/Exercicios.java
+++ src/main/java/com/api/exceptions/Exercicios.java
@@ -58,8 +58,31 @@ public class Exercicios {
             } catch (InputMismatchException e) {
                 System.out.println("Erro: " + e.getMessage());
                 ex2();
+                // chamando ex2 por que nao entra no try novamente
                 // Scanner nao pode ser fechado por conta do try catch
             }
+        }
+    }
+
+    public static void ex3() {
+        Scanner scanner = new Scanner(System.in);
+
+        String nomes[] = { "Lucas", "Enzo", "Eloisa" };
+
+        System.out.println("Escolha um número para o nome: ");
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println((i + 1) + " - " + nomes[i]);
+        }
+
+        try {
+            int escolha = scanner.nextInt() - 1;
+            try {
+                System.out.println("Nome escolhido: " + nomes[escolha]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Erro: " + e.getMessage());
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: " + e.getMessage());
         }
     }
 }
